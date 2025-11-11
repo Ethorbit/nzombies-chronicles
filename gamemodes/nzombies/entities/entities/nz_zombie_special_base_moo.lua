8afe966689df8a12d2996a3e3470b3cc4b14a1eb
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-11-10 18:11:31 -0800

    Remove all nZR zombies
    The nZR devs don't know how to code and everything they touch breaks. The only reason we used the moo base is because my admin Berb went ahead without my permission and added it. We will no longer be using anything nZR.

--- gamemodes/nzombies/entities/entities/nz_zombie_special_base_moo.lua
+++
@@ -1,6 +0,0 @@
--- Just gonna have to be a copy paste of nz_zombie_special_base since Gmod doesn't let us derive two different bases (at least I don't think it does..)
-AddCSLuaFile()
-
-ENT.Author = "Ethorbit"
-ENT.Base = "nz_zombiebase_moo"
-ENT.IgnoreDistractions = true -- Ignore stuff like monkey bombs, we almost always only care about the player.
