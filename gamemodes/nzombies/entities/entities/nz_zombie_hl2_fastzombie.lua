039c512ac03f98913bf552a84438ff1aef14fcc0
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-11-20 21:16:04 -0800

    Add license headers

--- gamemodes/nzombies/entities/entities/nz_zombie_hl2_fastzombie.lua
+++ gamemodes/nzombies/entities/entities/nz_zombie_hl2_fastzombie.lua
@@ -1,3 +1,23 @@
+--[[ LICENSE HEADER MANAGED BY add-license-header
+
+Copyright (C) 2014-2015 Alig96
+Copyright (C) 2015-2022 Zet0rz
+Copyright (C) 2020-2025 Ethorbit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 -- I made this as a tribute to the (no longer existing) Half-Life 2: Deathmatch Zombies (@ Phoneburnia) community
 -- It may not exist any more, but it was my childhood, and fighting waves of fastzombies with teammates was a very fun
 -- and unforgettable experience, I'm not letting that get lost to time.
--- gamemodes/nzombies/gamemode/anticheat/sh_cvars.lua
+++ gamemodes/nzombies/gamemode/anticheat/sh_cvars.lua
@@ -1,3 +1,23 @@
+--[[ LICENSE HEADER MANAGED BY add-license-header
+
+Copyright (C) 2014-2015 Alig96
+Copyright (C) 2015-2022 Zet0rz
+Copyright (C) 2020-2025 Ethorbit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 CreateConVar("nz_anticheat_delay", 0.0, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, 
 "The additional time to a server tick (0.1 would be 100ms) to scan for cheaters.")
 
